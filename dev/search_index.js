005ecccb062e3d6dbc750cb1ca82d22e7482973b
Documenter.jl <documenter@juliadocs.github.io>
2021-08-04 18:03:36 +0000

    build based on 018675d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Emerald.jl","page":"Home","title":"Emerald.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Refactored CliMA Land model.","category":"page"},{"location":"API/#Emerald","page":"API","title":"Emerald","text":"","category":"section"},{"location":"API/","page":"API","title":"API","text":"Refactored CliMA Land model.","category":"page"}]
+}
