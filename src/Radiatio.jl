1ff97e6a33c60aea0953f9ba2aac9a66de3c25dd
Yujie <jesiner@gmail.com>
2021-10-22 14:51:27 -0700

    add function to calculate leaf level SIF

--- src/Radiatio.jl
+++ src/Radiatio.jl
@@ -1,6 +1,15 @@
 module Radiatio
 
+using DocStringExtensions: METHODLIST
 using PkgUtility: H_PLANCK, LIGHT_SPEED, AVOGADRO
+
+
+# export public functions
+export average_transmittance, energy, photon
+
+
+# constants
+const FAC = 1e-9 / (H_PLANCK() * LIGHT_SPEED() * AVOGADRO());
 
 
 #######################################################################################################################################################################################################
@@ -67,21 +76,64 @@ end
 # Changes made to this function
 # General
 #     2021-Oct-22: rename the function to photon
+#     2021-Oct-22: add a method to convert direct from number to number
 #
 #######################################################################################################################################################################################################
-_FAC(FT) = FT(1e-9) / (H_PLANCK(FT) * LIGHT_SPEED(FT) * AVOGADRO(FT));
+"""
+This function convert energy to photons based the wavelength. Supported methods are
+
+$(METHODLIST)
+"""
+function photon end
 
 
 """
-    photon(λ::Array{FT}, E::Array{FT}) where {FT<:AbstractFloat}
+    photon(λ::FT, E::FT) where {FT<:AbstractFloat}
 
 Return the number of moles of photons, given
-- `λ` An array of wave length in `[nm]`, converted to `[m]` by _FAC
+- `λ` Wave length in `[nm]`, converted to `[m]` by FAC
 - `E` Joules of energy
+
+---
+# Examples
+```julia
+phot = photon(500.0, 0.1);
+phots = photon.([500.0, 400.0], [0.1, 0.1]);
+```
 """
-function photon(λ::Array{FT}, E::Array{FT}) where {FT<:AbstractFloat}
-    return (E .* λ) .* _FAC(FT)
-end
+photon(λ::FT, E::FT) where {FT<:AbstractFloat} = E * λ * FT(FAC);
+
+
+#######################################################################################################################################################################################################
+#
+# Changes made to this function
+# General
+#     2021-Oct-22: define function to convert photon back to energy
+#
+#######################################################################################################################################################################################################
+"""
+This function convert photons to energy based the wavelength. Supported methods are
+
+$(METHODLIST)
+"""
+function energy end
+
+
+"""
+    energy(λ::FT, phot::FT) where {FT<:AbstractFloat}
+
+Return the energy, given
+- `λ` Wave length in `[nm]`, converted to `[m]` by FAC
+- `phot` Number of moles of photon
+
+---
+# Examples
+```julia
+e = energy(500.0, 0.1);
+es = energy.([500.0, 400.0], [0.1, 0.1]);
+```
+"""
+energy(λ::FT, phot::FT) where {FT<:AbstractFloat} = phot / (λ * FT(FAC));
 
 
 end # module
